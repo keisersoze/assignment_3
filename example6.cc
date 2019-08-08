037d725b2c0462ead0deaf0d7fcda0ce52b41e8d
Veneri Alberto <veneri.alberto@gmail.com>
2019-08-08 15:59:12 +0200

    WIP test

--- example6.cc
+++ example6.cc
@@ -69,29 +69,35 @@ void print_matrix(matrix<T, h, w> &m) {
 
 int main() {
     int i = 0;
+
     matrix<int, 100, 100> mA;
     initializeCells(mA);
     matrix<int, 100, 100> mB;
     initializeCells(mB);
     matrix<int, 100, 100> mC;
     initializeCells(mC);
-
-    /*matrix<int, 100, 100> res1 = mC + mB;
-    print_matrix(res1);
-    matrix<int, 100, 100> res2 = mC + mB + mA;
-    print_matrix(res2);*/
-    matrix<int, 100, 100> res3 = mC + (mB + mA);
-    print_matrix(res3);
-    /*matrix<int, 100, 100> res4 = (mC + mB) + (mC + mB);
-    print_matrix(res4);
-
+    matrix<int, 100, 20> mD;
+    initializeCells(mC);
     matrix<int> m1(100, 100);
     initializeCells(m1);
     matrix<int> m2(100, 100);
     initializeCells(m2);
     matrix<int> m3(100, 100);
     initializeCells(m3);
-
+    /**
+     * Static matrix_sum
+     */
+    matrix<int, 100, 100> res1 = mC + mB;
+    print_matrix(res1);
+    matrix<int, 100, 100> res2 = mC + mB + mA;
+    print_matrix(res2);
+    matrix<int, 100, 100> res3 = mC + (mB + mA);
+    print_matrix(res3);
+    matrix<int, 100, 100> res4 = (mC + mB) + (mC + mB);
+    print_matrix(res4);
+    /**
+    * Dynamic matrix_sum
+    */
     matrix<int> res5 = m1 + m2;
     print_matrix(res5);
     matrix<int> res6 = m1 + m2 + m3;
@@ -100,11 +106,17 @@ int main() {
     print_matrix(res7);
     matrix<int> res8 = (m1 + m1) + (m1 + m1);
     print_matrix(res8);
-
+    /**
+    * Dynamic e static matrix_sum
+    */
     matrix<int> res9 = mA + m1;
     print_matrix(res9);
     matrix<int> res10 = m1 + (mA + m2);
     print_matrix(res10);
-*/
 
+    /**
+     * Static matrix_produc
+     */
+    matrix<int> res10 = m1 + (mA + m2);
+    print_matrix(res10);
 }
