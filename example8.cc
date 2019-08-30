4dafd78c900f952d0dd64162233e1fa30070817c
Filippo <filippomaganza@gmail.com>
2019-08-30 18:27:31 +0200

    test 4 somme

--- example8.cc
+++ example8.cc
@@ -28,17 +28,41 @@ void initializeCells(matrix<T, h, w> &m) {
         }
     }
 }
+template<typename T>
+void print_matrix(matrix<T> &m) {
+    for (int i = 0; i != m.get_height(); ++i) {
+        for (int j = 0; j != m.get_width(); ++j)
+            std::cout << m(i, j) << ' ';
+        std::cout << '\n';
+    }
+    std::cout << std::endl;
+}
+
+template<typename T, unsigned h, unsigned w>
+void print_matrix(matrix<T, h, w> &m) {
+    for (int i = 0; i != h; ++i) {
+        for (int j = 0; j != w; ++j)
+            std::cout << m(i, j) << ' ';
+        std::cout << '\n';
+    }
+    std::cout << std::endl;
+}
+
 
 int main() {
-    matrix<int, 3000, 3000> mA;
+    matrix<int, 15000, 15000> mA;
     initializeCells(mA);
-    matrix<int, 3000, 3000> mB;
+    matrix<int, 15000, 15000> mB;
+    initializeCells(mB);
+    matrix<int, 15000, 15000> mC;
+    initializeCells(mA);
+    matrix<int, 15000, 15000> mD;
     initializeCells(mB);
 
 
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-    matrix<int, 3000, 3000> multiplication = mA * mB;
-    int first = multiplication(0, 0);
+    matrix<int, 15000, 15000> multiplication = mA + mB + mC + mD;
+    int first = multiplication(345, 456);
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
     std::cout << "The first element is " << first << ", and it took "
               << std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count() / 1000.0 << " seconds"
